b999caf5d42a58d38261c712e7cd6e2e2cc88f61
Ray <53952835+rheinzelman@users.noreply.github.com>
2021-11-09 13:49:09 -0800

    humanVsCPU update 1
    Player can now choose which side of the board they would like to be.
    
    Also created a new file called ioDriver which currently only has a function for converting the python-chess board printing function into a readable 2d array. In the future this will be used for GUI implementation.

--- python-chess-pkg/humanVSCPU.py
+++ python-chess-pkg/humanVSCPU.py
@@ -15,49 +15,94 @@ import chess.svg
 #defines the engine object that will be used to generate CPU moves
 engine = chess.engine.SimpleEngine.popen_uci('src/stockfish')
 
-#split() helps us break the board's ascii output into a 2d array for gui utilization
-def split(ASCIIBoard):
-    return [char for char in ASCIIBoard]
-
 #initialize a new Board state called board
 board = chess.Board()
+
+#let the player choose which side they would like to play
+print("What side would you like to play as: (W/B)")
+while 1:
+    playerColor = input()
+    if(playerColor == 'W' or playerColor == 'B'):
+        break
+    print('Please only enter W or B')
 
 #keep track if the player has resigned
 resignFlag = False
 
-#while there is no game ending condition, or the player has not resigned, continue normal play
-while(board.is_game_over() == False and resignFlag == False):
-    #print the initial board state
-    print(board)
-    print('')
-    #generate a move for the CPU
-    CPUMove = engine.play(board, chess.engine.Limit(time=0.1))
-    #sleep some time to give the illusion of playing a human opponent
-    time.sleep(.5)
-    #push the move to the board
-    board.push(CPUMove.move)
-    #print the board again so the player can see the CPU's move
-    print(board)
-    print('')
-    #create a new flag for legal moves
-    isLegalMove = False
-    #while we have yet to receive a legal move, and the player has not resigned,
-    while(isLegalMove == False and resignFlag == False):
-        #take some user input
-        user_input = input()
-        #if the user input is not resign
-        if(user_input != 'resign'):
-            #create a Move.from_uci variable called move
-            move = chess.Move.from_uci(user_input)
-        else:
-            #if the player did resign, set the resign flag to true
-            resignFlag = True
-            print('Player has resigned, CPU wins!')
-        #if the move is in the dynamic list of legal moves,
-        #set the legal move? flag to true, and push the move to the board
-        if(move in board.legal_moves):
-            isLegalMove = True
-            board.push(move)    
+if(playerColor == 'W'):
+    while(board.is_game_over() == False and resignFlag == False):
+        print(board)
+        print('')
+
+        #create a new flag for legal moves
+        isLegalMove = False
+        #while we have yet to receive a legal move, and the player has not resigned,
+        while(isLegalMove == False and resignFlag == False):
+            #take some user input
+            print('Your move: ', end='')
+            user_input = input()
+            #if the user input is not resign
+            if(user_input != 'resign'):
+                #create a Move.from_uci variable called move
+                move = chess.Move.from_uci(user_input)
+            else:
+                #if the player did resign, set the resign flag to true
+                resignFlag = True
+                print('Player has resigned, CPU wins!')
+            #if the move is in the dynamic list of legal moves,
+            #set the legal move? flag to true, and push the move to the board
+            if(move in board.legal_moves):
+                isLegalMove = True
+                board.push(move)
+        print(board)
+        print('')
+        #generate a move for the CPU
+        CPUMove = engine.play(board, chess.engine.Limit(time=0.1))
+        #sleep some time to give the illusion of playing a human opponent
+        time.sleep(.5)
+        print('CPU move: ', end='')
+        print(CPUMove.move)
+        #push the move to the board, .move just converts into a push() readable format
+        board.push(CPUMove.move)
+        #print the board again so the player can see the CPU's move
+
+if(playerColor == 'B'):
+    #while there is no game ending condition, or the player has not resigned, continue normal play
+    while(board.is_game_over() == False and resignFlag == False):
+        #print the initial board state
+        print(board)
+        print('')
+        #generate a move for the CPU
+        CPUMove = engine.play(board, chess.engine.Limit(time=0.1))
+        #sleep some time to give the illusion of playing a human opponent
+        time.sleep(.5)
+        print('CPU move: ', end='')
+        print(CPUMove.move)
+        #push the move to the board, .move just converts into a push() readable format
+        board.push(CPUMove.move)
+        #print the board again so the player can see the CPU's move
+        print(board)
+        print('')
+        #create a new flag for legal moves
+        isLegalMove = False
+        #while we have yet to receive a legal move, and the player has not resigned,
+        while(isLegalMove == False and resignFlag == False):
+            #take some user input
+            print('Your move: ', end='')
+            user_input = input()
+            #if the user input is not resign
+            if(user_input != 'resign'):
+                #create a Move.from_uci variable called move
+                move = chess.Move.from_uci(user_input)
+            else:
+                #if the player did resign, set the resign flag to true
+                resignFlag = True
+                print('Player has resigned, CPU wins!')
+            #if the move is in the dynamic list of legal moves,
+            #set the legal move? flag to true, and push the move to the board
+            if(move in board.legal_moves):
+                isLegalMove = True
+                board.push(move)    
 
     '''
     placeholder for how we will send the board's character array out for gui processing
---
+++ python-chess-pkg/ioDriver.py
@@ -0,0 +1,31 @@
+#helps us break the board's ascii output into a 2d array for gui and IO utilization
+def formatASCII(ASCIIBoard):
+    #initialize an empty char array to represent the ascii board
+    boardArray = [
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','','']
+    ]
+    
+    #take the board object and make it a string
+    #reformat it to be only piece and space characters
+    #make a list with each element being an array from the board object string
+    reformattedASCII = str(board)
+    reformattedASCII = reformattedASCII.replace(' ', '')
+    reformattedASCII = reformattedASCII.replace('\n', '')
+    charList = [char for char in reformattedASCII]
+
+    #iterate through the boardArray and assign each element it's corresponding charList Element
+    charListIndex = 0
+    for i in range(0,8):
+        for j in range(0,8):
+            boardArray[i][j] = charList[charListIndex]
+            charListIndex = charListIndex + 1
+
+    #return an I/O compatible boardArray
+    return boardArray
